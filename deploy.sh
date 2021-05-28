570cd5f93b5577b0a0b8147ed16f613d9ebaf2b9
fboltz <frederic.boltz@gmail.com>
2021-05-28 17:34:23 +0200

    Increase version  number

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 vagrant plugin uninstall vagrant-multipass
-rm vagrant-multipass-0.1.0.gem
+rm vagrant-multipass-0.1.1.gem
 gem build vagrant-multipass.gemspec
-vagrant plugin install vagrant-multipass-0.1.0.gem
+vagrant plugin install vagrant-multipass-0.1.1.gem
