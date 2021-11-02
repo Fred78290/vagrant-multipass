010f182fa828f060f5fad348c1a70039df433cc2
fboltz <frederic.boltz@gmail.com>
2021-11-02 11:38:20 +0100

    Fix Issue #1

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 vagrant plugin uninstall vagrant-multipass
-rm vagrant-multipass-0.1.1.gem
+rm vagrant-multipass-0.1.2.gem
 gem build vagrant-multipass.gemspec
-vagrant plugin install vagrant-multipass-0.1.1.gem
+vagrant plugin install vagrant-multipass-0.1.2.gem
